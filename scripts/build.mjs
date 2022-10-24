31ee297dc711150f5bde308a766606823fccd74e
wanchun <445436867@qq.com>
2022-10-24 09:55:54 +0800

    feat: vite构建

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,9 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { execa } from 'execa';
-
-const args = process.argv.slice(2);
-
-const isDev = args.includes('dev');
+import { build } from 'vite';
+import fse from 'fs-extra';
+import dts from 'vite-plugin-dts';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -21,14 +20,42 @@ const pkgs = [
     'plugin-designer',
 ];
 
-const run = (bin, args, opts = {}) =>
-    execa(bin, args, { stdio: 'inherit', ...opts });
-
 for (const pkg of pkgs) {
     (async () => {
-        const entry = path.resolve(__dirname, `../packages/${pkg}`);
-        await run('father', [isDev ? 'dev' : 'build'], {
-            cwd: entry,
-        });
+        let entry = path.resolve(__dirname, `../packages/${pkg}/src/index.ts`);
+        if (!fse.pathExistsSync(entry)) {
+            entry = path.resolve(__dirname, `../packages/${pkg}/src/index.tsx`);
+        }
+        if (fse.pathExistsSync(entry)) {
+            await build({
+                root: path.resolve(__dirname, `../packages/${pkg}`),
+                build: {
+                    minify: false,
+                    outDir: 'es',
+                    lib: {
+                        entry,
+                        formats: ['es'],
+                    },
+                    rollupOptions: {
+                        // 确保外部化处理那些你不想打包进库的依赖
+                        external: [/^@?[a-zA-Z]/],
+                        makeAbsoluteExternalsRelative: false,
+                        output: {
+                            // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                            globals: {
+                                vue: 'Vue',
+                            },
+                        },
+                    },
+                },
+                plugins: [
+                    vueJsx(),
+                    dts({
+                        entryRoot: 'src',
+                        outputDir: 'es',
+                    }),
+                ],
+            });
+        }
     })();
 }

b49339a907cca91b08a3b88dbda3944511088e7d
winixt <haizekuo@gmail.com>
2022-12-13 20:32:09 +0800

    feat: 生成使用的 packages

--- packages/plugin-code-generator/src/exportBtn.tsx
+++ packages/plugin-code-generator/src/exportBtn.tsx
@@ -1,12 +1,29 @@
 import { defineComponent } from 'vue';
-import { project } from '@webank/letgo-engine';
-import { TransformStage } from '@webank/letgo-types';
-import { FButton } from '@fesjs/fes-design';
+import { project, material } from '@webank/letgo-engine';
+import { TransformStage, isProCodeComponentType } from '@webank/letgo-types';
+import { FButton, FMessage } from '@fesjs/fes-design';
 
 export default defineComponent({
     setup() {
         const genCode = () => {
+            const packages = material.getAssets().packages;
             const schema = project.getSchema(TransformStage.Save);
+            const usedPackages = [];
+            for (const component of schema.componentsMap) {
+                if (isProCodeComponentType(component)) {
+                    const pkg = packages.find(
+                        (pkg) =>
+                            pkg.package === component.package &&
+                            pkg.version === component.version,
+                    );
+                    if (!pkg) {
+                        FMessage.error('组件版本匹配异常，请联系开发处理');
+                        return;
+                    }
+                    usedPackages.push(pkg);
+                }
+            }
+            console.log(usedPackages);
             console.log(schema);
         };
         return () => {

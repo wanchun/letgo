cdb116e304c0aa98554f5b36dc5ec6f32aba8d9c
wanchun <445436867@qq.com>
2022-12-13 18:47:19 +0800

    feat: EventSetter 删除选择选项

--- packages/engine-ext/src/setter/event-setter/index.css.ts
+++ packages/engine-ext/src/setter/event-setter/index.css.ts
@@ -1,3 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
-export const wrapperCls = style({});
+export const pointerCls = style({
+    cursor: 'pointer',
+});
+
+export const lightCls = style({
+    color: 'rgba(0, 0, 0, 0.5)',
+});
--- packages/engine-ext/src/setter/event-setter/index.tsx
+++ packages/engine-ext/src/setter/event-setter/index.tsx
@@ -10,6 +10,7 @@ import {
 } from '@fesjs/fes-design';
 import { Setting, Delete } from '@icon-park/vue-next';
 import { commonProps } from '../../common/setter-props';
+import { pointerCls, lightCls } from './index.css';
 
 const DEFINITION_EVENT_TYPE = {
     EVENTS: 'events',
@@ -25,11 +26,28 @@ type EventDefinition = {
     list: EventList;
 };
 
-type Events = Array<{
+type OptionList = Array<{
     value: string;
     label: string;
-    list: EventList;
+    description?: string;
 }>;
+
+type EventType = {
+    value: string;
+    label: string;
+    list: OptionList;
+    choose: string[];
+};
+
+function transformList(list: EventList): OptionList {
+    return list.map((event) => {
+        return {
+            value: event.name,
+            label: event.name,
+            description: event.description,
+        };
+    });
+}
 
 const EventSetterView = defineComponent({
     name: 'EventSetterView',
@@ -42,36 +60,62 @@ const EventSetterView = defineComponent({
     },
     setup(props) {
         console.log(props);
-        const eventData: Ref<Events> = ref([]);
+        const eventData: Ref<EventType[]> = ref([]);
+
+        const selectedEventData = ref([]);
+
+        watch(
+            eventData,
+            () => {
+                let arr: Array<{ name: string }> = [];
+                eventData.value.forEach((item) => {
+                    arr = arr.concat(
+                        item.choose.map((name) => {
+                            return {
+                                name,
+                                target: item,
+                            };
+                        }),
+                    );
+                });
+                selectedEventData.value = arr;
+            },
+            {
+                deep: true,
+            },
+        );
 
         watch(
             () => props.definition,
             () => {
-                const events: Events = [];
+                const events: EventType[] = [];
                 props.definition.forEach((item) => {
                     if (item.type === DEFINITION_EVENT_TYPE.LIFE_CYCLE_EVENT) {
-                        const eventItem = {
+                        const eventItem: EventType = {
                             value: item.type,
                             label: '生命周期',
-                            list: item.list || [],
+                            list: transformList(item.list || []),
+                            choose: [],
                         };
                         events.push(eventItem);
                     }
 
                     if (item.type === DEFINITION_EVENT_TYPE.EVENTS) {
-                        const eventItem = {
+                        const eventItem: EventType = {
                             value: item.type,
                             label: '组件自带事件',
-                            list: item.list || [],
+                            list: transformList(item.list || []),
+                            choose: [],
                         };
                         events.push(eventItem);
                     }
 
                     if (item.type === DEFINITION_EVENT_TYPE.NATIVE_EVENTS) {
-                        const eventItem = {
+                        const eventItem: EventType = {
                             value: item.type,
                             label: '原生事件',
-                            list: item.list || [],
+                            list: transformList(item.list || []),
+                            choose: [],
                         };
                         events.push(eventItem);
                     }
@@ -82,6 +126,14 @@ const EventSetterView = defineComponent({
                 immediate: true,
             },
         );
+
+        const handleDelete = (row: any) => {
+            const { name, target } = row;
+            const index = target.choose.indexOf(name);
+            if (index !== -1) {
+                target.choose.splice(index, 1);
+            }
+        };
 
         onMounted(() => {
             props.onMounted?.();
@@ -96,13 +148,8 @@ const EventSetterView = defineComponent({
                                 <FFormItem label={item.label}>
                                     <FSelect
                                         multiple
-                                        options={item.list.map((event) => {
-                                            return {
-                                                value: event.name,
-                                                label: event.name,
-                                                description: event.description,
-                                            };
-                                        })}
+                                        v-model={item.choose}
+                                        options={item.list}
                                         v-slots={{
                                             option: ({
                                                 value,
@@ -114,7 +161,7 @@ const EventSetterView = defineComponent({
                                                 return (
                                                     <FSpace justify="space-between">
                                                         <span>{value}</span>
-                                                        <span>
+                                                        <span class={lightCls}>
                                                             {description}
                                                         </span>
                                                     </FSpace>
@@ -126,20 +173,28 @@ const EventSetterView = defineComponent({
                             );
                         })}
                     </FForm>
-                    <FTable bordered>
+                    <FTable data={selectedEventData.value} bordered>
                         <FTableColumn
                             prop="name"
                             label="已有事件"
                         ></FTableColumn>
                         <FTableColumn
                             label="操作"
+                            width={80}
                             v-slots={{
-                                default: () => {
+                                default: ({ row }: { row: any }) => {
                                     return (
-                                        <>
-                                            <Setting></Setting>
-                                            <Delete></Delete>
-                                        </>
+                                        <FSpace justify="space-between">
+                                            <Setting
+                                                class={pointerCls}
+                                            ></Setting>
+                                            <Delete
+                                                onClick={() => {
+                                                    handleDelete(row);
+                                                }}
+                                                class={pointerCls}
+                                            ></Delete>
+                                        </FSpace>
                                     );
                                 },
                             }}

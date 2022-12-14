3cb983b5d8508ef8a1e05fc9f12237a86af538f1
winixt <haizekuo@gmail.com>
2022-12-14 19:58:05 +0800

    feat: 代码生成添加组件导入

--- packages/plugin-code-generator/src/genCode/index.ts
+++ packages/plugin-code-generator/src/genCode/index.ts
@@ -1,20 +1,76 @@
-import { ProjectSchema, RootSchema } from '@webank/letgo-types';
+import {
+    ProjectSchema,
+    isNodeSchema,
+    NodeData,
+    ComponentMap,
+    RootSchema,
+    isJSSlot,
+} from '@webank/letgo-types';
+import { isArray } from 'lodash-es';
 import { genPageTemplate } from './genTemplate';
 import { genStyle } from './style';
 import { genScript } from './script';
 
-function compileRootSchema(rootSchema: RootSchema) {
+function compileRootSchema(
+    componentMaps: ComponentMap[],
+    rootSchema: RootSchema,
+) {
     if (rootSchema.componentName === 'Page') {
         return {
             template: genPageTemplate(rootSchema),
-            script: genScript(rootSchema),
+            script: genScript(componentMaps, rootSchema),
             style: genStyle(rootSchema),
         };
     }
     return {};
 }
 
+function getUseComponentNames(
+    nodeData: NodeData[],
+    componentNames: Set<string> = new Set(),
+) {
+    nodeData.forEach((item) => {
+        if (isNodeSchema(item)) {
+            componentNames.add(item.componentName);
+            if (item.props.children) {
+                if (isArray(item.props.children)) {
+                    getUseComponentNames(item.props.children, componentNames);
+                } else {
+                    getUseComponentNames([item.props.children], componentNames);
+                }
+            }
+            if (item.children) {
+                getUseComponentNames(item.children, componentNames);
+            }
+        } else if (isJSSlot(item)) {
+            getUseComponentNames(item.value.components, componentNames);
+        }
+    });
+    return componentNames;
+}
+
+function getUseComponents(
+    componentMaps: ComponentMap[],
+    rootSchema: RootSchema,
+) {
+    const componentNames = getUseComponentNames(rootSchema.children);
+    const useComponents: ComponentMap[] = [];
+    for (const componentName of componentNames.values()) {
+        const component = componentMaps.find(
+            (item) => item.componentName === componentName,
+        );
+        if (component) useComponents.push(component);
+    }
+
+    return useComponents;
+}
+
 export function schemaToCode(schema: ProjectSchema) {
-    const rootComponents = schema.componentsTree.map(compileRootSchema);
+    const rootComponents = schema.componentsTree.map((rootSchema) => {
+        compileRootSchema(
+            getUseComponents(schema.componentsMap, rootSchema),
+            rootSchema,
+        );
+    });
     console.log(rootComponents);
 }
--- packages/plugin-code-generator/src/genCode/script.ts
+++ packages/plugin-code-generator/src/genCode/script.ts
@@ -1,8 +1,36 @@
-import { RootSchema } from '@webank/letgo-types';
+import {
+    RootSchema,
+    ComponentMap,
+    NpmInfo,
+    isProCodeComponentType,
+} from '@webank/letgo-types';
 
-export function genScript(rootSchema: RootSchema) {
+function genComponentImports(componentMaps: ComponentMap[]) {
+    const pkgs: Record<string, NpmInfo[]> = {};
+    componentMaps.forEach((componentMap) => {
+        if (isProCodeComponentType(componentMap)) {
+            if (pkgs[componentMap.package]) {
+                pkgs[componentMap.package].push(componentMap);
+            } else {
+                pkgs[componentMap.package] = [componentMap];
+            }
+        }
+    });
+
+    return Object.keys(pkgs).map((pkg) => {
+        return `import {${pkgs[pkg]
+            .map((item) => item.componentName)
+            .join(', ')}} from '${pkg}';`;
+    });
+}
+
+export function genScript(
+    componentMaps: ComponentMap[],
+    rootSchema: RootSchema,
+) {
     if (rootSchema.code) {
         return `<script setup>
+            ${genComponentImports(componentMaps)}
             ${rootSchema.code || ''}
         </script>`;
     }

c03149aa9f21cf5c372df218be63d1f83da58d98
wanchun <445436867@qq.com>
2023-05-25 15:54:41 +0800

    refactor: NodeChildren中响应式状态改为使用markReactive

--- packages/designer/src/node/node-children.ts
+++ packages/designer/src/node/node-children.ts
@@ -1,9 +1,7 @@
 import { EventEmitter } from 'eventemitter3';
 import type { IPublicTypeNodeData } from '@webank/letgo-types';
 import { IPublicEnumTransformStage, isNodeSchema } from '@webank/letgo-types';
-import { shallowEqual } from '@webank/letgo-utils';
-import type { ShallowRef } from 'vue';
-import { shallowRef, triggerRef } from 'vue';
+import { markComputed, markReactive, shallowEqual } from '@webank/letgo-utils';
 import type { INode } from '../types';
 
 interface IOnChangeOptions {
@@ -12,7 +10,7 @@ interface IOnChangeOptions {
 }
 
 export class NodeChildren {
-    private children: ShallowRef<INode[]> = shallowRef([]);
+    private children: INode[];
 
     private emitter = new EventEmitter();
 
@@ -20,12 +18,16 @@ export class NodeChildren {
      * 元素个数
      */
     get size(): number {
-        return this.children.value.length;
+        return this.children.length;
     }
 
     constructor(readonly owner: INode, data?: IPublicTypeNodeData | IPublicTypeNodeData[]) {
+        markReactive(this, {
+            children: [],
+        });
+        markComputed(this, ['size']);
         if (data) {
-            this.children.value = (Array.isArray(data) ? data : [data]).map(
+            this.children = (Array.isArray(data) ? data : [data]).map(
                 (child) => {
                     return this.owner.document.createNode(child);
                 },
@@ -34,7 +36,7 @@ export class NodeChildren {
     }
 
     export(stage: IPublicEnumTransformStage = IPublicEnumTransformStage.Save): IPublicTypeNodeData[] {
-        return this.children.value.map((node) => {
+        return this.children.map((node) => {
             const data = node.export(stage);
             if (node.isLeaf() && IPublicEnumTransformStage.Save === stage)
                 return data.children as IPublicTypeNodeData;
@@ -46,8 +48,8 @@ export class NodeChildren {
     import(data?: IPublicTypeNodeData | IPublicTypeNodeData[]) {
         data = data ? (Array.isArray(data) ? data : [data]) : [];
 
-        const originChildren = this.children.value.slice();
-        this.children.value.forEach(child => child.setParent(null));
+        const originChildren = this.children.slice();
+        this.children.forEach(child => child.setParent(null));
 
         const children = new Array<INode>(data.length);
         for (let i = 0, l = data.length; i < l; i++) {
@@ -69,39 +71,39 @@ export class NodeChildren {
             children[i] = node;
         }
 
-        this.children.value = children;
+        this.children = children;
         this.initParent();
         if (!shallowEqual(children, originChildren))
             this.emitter.emit('change');
     }
 
     initParent() {
-        this.children.value.forEach(child => child.setParent(this.owner));
+        this.children.forEach(child => child.setParent(this.owner));
     }
 
     /**
      * 取得节点索引编号
      */
     indexOf(node: INode): number {
-        return this.children.value.indexOf(node);
+        return this.children.indexOf(node);
     }
 
     /**
      * 根据索引获得节点
      */
     get(index: number): INode | null {
-        return this.children.value.length > index
-            ? this.children.value[index]
+        return this.children.length > index
+            ? this.children[index]
             : null;
     }
 
     unlinkChild(node: INode) {
-        const i = this.children.value.indexOf(node);
+        const i = this.children.indexOf(node);
         if (i < 0)
             return;
 
-        this.children.value.splice(i, 1);
-        triggerRef(this.children);
+        // TODO: 可能不触发响应
+        this.children.splice(i, 1);
         this.emitter.emit('change', {
             type: 'unlink',
             node,
@@ -120,7 +122,7 @@ export class NodeChildren {
      */
     deleteChild(node: INode, purge = false) {
         // 需要在从 children 中删除 node 前记录下 index，internalSetParent 中会执行删除(unlink)操作
-        const i = this.children.value.indexOf(node);
+        const i = this.children.indexOf(node);
         if (purge) {
             node.setParent(null);
             try {
@@ -139,8 +141,8 @@ export class NodeChildren {
         });
         // purge 为 true 时，已在 internalSetParent 中删除了子节点
         if (i > -1 && !purge) {
-            this.children.value.splice(i, 1);
-            triggerRef(this.children);
+            // TODO: 可能不触发响应
+            this.children.splice(i, 1);
         }
     }
 
@@ -149,16 +151,16 @@ export class NodeChildren {
      */
     insertChild(node: INode, at?: number | null): void {
         const { children } = this;
-        let index = (at == null || at === -1) ? children.value.length : at;
+        let index = (at == null || at === -1) ? children.length : at;
 
-        const i = children.value.indexOf(node);
+        const i = children.indexOf(node);
 
         if (i < 0) {
-            if (index < children.value.length)
-                children.value.splice(index, 0, node);
+            if (index < children.length)
+                children.splice(index, 0, node);
 
             else
-                children.value.push(node);
+                children.push(node);
 
             node.setParent(this.owner);
         }
@@ -169,11 +171,11 @@ export class NodeChildren {
             if (index === i)
                 return;
 
-            children.value.splice(i, 1);
-            children.value.splice(index, 0, node);
+            children.splice(i, 1);
+            children.splice(index, 0, node);
         }
 
-        triggerRef(this.children);
+        // TODO: 可能不触发响应
 
         this.emitter.emit('change', {
             type: 'insert',
@@ -195,7 +197,7 @@ export class NodeChildren {
             return;
 
         this.purged = true;
-        this.children.value.forEach((child) => {
+        this.children.forEach((child) => {
             child.purge();
         });
     }

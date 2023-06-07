66aece38cc806ddc8cb3bea2f06a08cd2dea60aa
wanchun <445436867@qq.com>
2023-06-07 09:44:31 +0800

    fix: 优化

--- packages/designer/src/transducers/addon-combine.ts
+++ packages/designer/src/transducers/addon-combine.ts
@@ -198,7 +198,7 @@ export default function (
                     },
                 ],
                 extraProps: {
-                    display: 'accordion',
+                    display: 'block',
                 },
             });
         }

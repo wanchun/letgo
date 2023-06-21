f2aeff013aa3c006e0dce7003391a09e51df2917
winixt <haizekuo@gmail.com>
2023-06-21 19:55:31 +0800

    fix: 修复样式问题

--- packages/plugin-code/src/panel.css.ts
+++ packages/plugin-code/src/panel.css.ts
@@ -7,10 +7,12 @@ export const panelCls = style({
 
 export const leftPanelCls = style({
     width: '280px',
+    flexShrink: 0,
     height: '100%',
     borderRight: '1px solid #ebebeb',
 });
 
 export const rightPanelCls = style({
     flex: 1,
+    width: 0,
 });

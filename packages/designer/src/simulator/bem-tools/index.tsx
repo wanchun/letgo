5a817b5985ab2290677ac6793788165a500becbe
wanchun <445436867@qq.com>
2023-07-28 11:19:31 +0800

    feat: 优化拖拽边框

--- packages/designer/src/simulator/bem-tools/index.tsx
+++ packages/designer/src/simulator/bem-tools/index.tsx
@@ -1,5 +1,6 @@
 import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
+import { defineComponent, onBeforeUnmount, ref } from 'vue';
+import { editor } from '@harrywan/letgo-editor-core';
 import type { Simulator } from '../simulator';
 import { BorderDetectingView } from './border-detecting';
 import { BorderSelectingView } from './border-selecting';
@@ -16,6 +17,16 @@ export const BemToolsView = defineComponent({
     setup(props) {
         const { simulator } = props;
 
+        const isPointerEvents = ref(false);
+
+        const callback = editor.onGot('isSkeletonDragging', (val) => {
+            isPointerEvents.value = val;
+        });
+
+        onBeforeUnmount(() => {
+            callback?.();
+        });
+
         return () => {
             if (simulator.designMode === 'live')
                 return null;
@@ -29,6 +40,7 @@ export const BemToolsView = defineComponent({
                         transform: `translate(${-scrollX * scale}px,${
                             -scrollY * scale
                         }px)`,
+                        pointerEvents: isPointerEvents.value ? 'inherit' : 'none',
                     }}
                 >
                     <BorderDetectingView key="hovering" simulator={simulator} />
--- packages/editor-skeleton/src/layouts/right-area.tsx
+++ packages/editor-skeleton/src/layouts/right-area.tsx
@@ -1,6 +1,7 @@
 import type { PropType } from 'vue';
 import { defineComponent, ref } from 'vue';
 import { useEventListener } from '@vueuse/core';
+import { editor } from '@harrywan/letgo-editor-core';
 import type { Area } from '../area';
 import type { IPanelConfig } from '../types';
 import type { Panel } from '../widget';
@@ -15,18 +16,30 @@ export default defineComponent({
     },
     setup(props) {
         const isActive = ref(false);
+        const isHover = ref(false);
         const width = ref<number>(400);
         let clientX = 0;
         let lasWidth = width.value;
+        let timer: number;
+        const onMouseenter = () => {
+            timer = setTimeout(() => {
+                isHover.value = true;
+            }, 300) as unknown as number;
+        };
+        const onMouseleave = () => {
+            isHover.value = false;
+            if (timer)
+                clearTimeout(timer);
+        };
         const onMousedown = (event: MouseEvent) => {
             isActive.value = true;
             clientX = event.clientX;
             lasWidth = width.value;
+            editor.set('isSkeletonDragging', true);
         };
         const onMousemove = (event: MouseEvent) => {
             if (!isActive.value)
                 return;
-
             width.value = lasWidth - (event.clientX - clientX);
         };
         const onMouseup = () => {
@@ -35,6 +48,7 @@ export default defineComponent({
 
             clientX = 0;
             isActive.value = false;
+            editor.set('isSkeletonDragging', false);
         };
         useEventListener(window.document, 'mousemove', onMousemove);
         useEventListener(window.document, 'mouseup', onMouseup);
@@ -50,7 +64,9 @@ export default defineComponent({
                     }}>
                     {area.items.map(item => item.content)}
                     <div
-                        class={[rightDividerCls, isActive.value && rightActiveDividerCls]}
+                        class={[rightDividerCls, (isHover.value || isActive.value) && rightActiveDividerCls]}
+                        onMouseenter={onMouseenter}
+                        onMouseleave={onMouseleave}
                         onMousedown={onMousedown}
                     ></div>
                 </div>

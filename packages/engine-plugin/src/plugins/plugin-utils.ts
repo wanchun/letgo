738c7744cde67468148fb676005740dbd9e90c2d
winixt <haizekuo@gmail.com>
2024-01-13 09:01:50 +0800

    fix: lodash to lodash-es

--- packages/engine-plugin/src/plugins/plugin-utils.ts
+++ packages/engine-plugin/src/plugins/plugin-utils.ts
@@ -1,4 +1,4 @@
-import { isPlainObject } from 'lodash';
+import { isPlainObject } from 'lodash-es';
 import type { IPluginPreferenceDeclaration } from './plugin-types';
 
 export function isValidPreferenceKey(

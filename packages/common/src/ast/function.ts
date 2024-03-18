8e57d373c8c86ee7e31f8cfbf173863ad12626e0
winixt <haizekuo@gmail.com>
2024-03-18 11:40:45 +0800

    fix: 添加 replaceFunctionName 的日志输出

--- packages/common/src/ast/function.ts
+++ packages/common/src/ast/function.ts
@@ -4,9 +4,15 @@ import { innerParse } from './ast';
 export function replaceFunctionName(code: string, name: string) {
     if (!code)
         return code;
-    const ast: any = innerParse(code);
-    if (ast.body[0].type === 'FunctionDeclaration')
-        ast.body[0].id.name = name;
+    try {
+        const ast: any = innerParse(code);
+        if (ast.body[0].type === 'FunctionDeclaration')
+            ast.body[0].id.name = name;
 
-    return generate(ast);
+        return generate(ast);
+    }
+    catch (err) {
+        console.log(code, err);
+        return code;
+    }
 }

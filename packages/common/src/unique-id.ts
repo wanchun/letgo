2da28e16ad6145be18e56a7d3bd814fd45e8d259
winixt <haizekuo@gmail.com>
2024-03-30 17:44:48 +0800

    fix: 优化 unique id 的生成

--- packages/common/src/unique-id.ts
+++ packages/common/src/unique-id.ts
@@ -1,4 +1,4 @@
 let guid = Date.now();
 export function uniqueId(prefix = '') {
-    return `${prefix}${(guid++).toString(36).toLowerCase()}${(Math.random() * 10000).toString(36).split('.')[0]}`;
+    return `${prefix ? `${prefix}_` : ''}${(guid++).toString(36).toLowerCase()}_${(Math.random() * 10000).toString(36).split('.')[0]}`;
 }

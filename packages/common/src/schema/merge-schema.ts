e8172154f86cf38fdf4f49f563c1a7404b245648
winixt <haizekuo@gmail.com>
2024-03-31 18:12:48 +0800

    chore: 完善部分 merge 功能

--- packages/common/src/schema/merge-schema.ts
+++ packages/common/src/schema/merge-schema.ts
@@ -1,5 +1,5 @@
-import type { ICodeItem, ICodeStruct, IEnumCodeType, IPublicTypeNodeSchema, IPublicTypePageSchema } from '@webank/letgo-types';
-import { isEqual } from 'lodash-es';
+import type { ICodeItem, ICodeStruct, IEnumCodeType, IPublicTypeNodeSchema, IPublicTypePageSchema, IPublicTypeRootSchema } from '@webank/letgo-types';
+import { isEqual, omit } from 'lodash-es';
 import { traverseNodeSchema } from './traverse-schema';
 
 enum DiffType {
@@ -66,6 +66,8 @@ function calcCodeModify(baseCodeMap: Map<string, ICodeItem>, targetCodeMap: Map<
             });
         }
     }
+
+    return diffResult;
 }
 
 function getCodeId(codeDiff: CodeDiff, diffMap: Map<string, CodeDiff>) {
@@ -79,13 +81,25 @@ function getCodeId(codeDiff: CodeDiff, diffMap: Map<string, CodeDiff>) {
 interface CodeConflict {
     key: string;
     id: string;
-    type: IEnumCodeType;
-    currentId: string;
-    currentType: IEnumCodeType;
     diffType: DiffType;
+    currentCode?: ICodeItem;
+    currentNode?: IPublicTypeNodeSchema;
 }
 
-function getCodeModifyConflict(currentDiffMap: Map<string, CodeDiff>, nextDiffMap: Map<string, CodeDiff>) {
+/**
+ * 判定冲突条件
+ * 1. 同时新增了相同的 id，并且类型不一样，类型一样会进行覆盖
+ * 2. 同时修改了 id, 并且修改的名字不一样
+ */
+function getCodeModifyConflict({
+    currentDiffMap,
+    nextDiffMap,
+    currentCodeMap,
+}: {
+    currentDiffMap: Map<string, CodeDiff>;
+    nextDiffMap: Map<string, CodeDiff>;
+    currentCodeMap: Map<string, ICodeItem>;
+}) {
     const conflictMap = new Map<string, CodeConflict>();
 
     for (const [key, value] of nextDiffMap) {
@@ -94,22 +108,33 @@ function getCodeModifyConflict(currentDiffMap: Map<string, CodeDiff>, nextDiffMa
             conflictMap.set(key, {
                 key,
                 id: currentDiff.id,
-                currentId: currentDiff.nextId,
-                type: value.type,
                 diffType: DiffType.Updated,
+                currentCode: currentCodeMap.get(key),
             });
         }
         else if (value.diffType === DiffType.Added) {
-
+            const currentDiff = getCodeId(value, currentDiffMap);
+            if (currentDiff) {
+                conflictMap.set(key, {
+                    key,
+                    id: currentDiff.id,
+                    diffType: DiffType.Updated,
+                    currentCode: currentCodeMap.get(currentDiff.key),
+                });
+            }
         }
     }
+
+    return conflictMap;
 }
 
-function genNodeMap(schema: IPublicTypePageSchema) {
+function genNodeMap(schema: IPublicTypeRootSchema) {
     const nodeMap = new Map<string, IPublicTypeNodeSchema>();
-    traverseNodeSchema(schema.children, (node: IPublicTypeNodeSchema) => {
-        nodeMap.set(node.id, node);
-    });
+    if (schema.children) {
+        traverseNodeSchema(schema.children, (node: IPublicTypeNodeSchema) => {
+            nodeMap.set(node.id, node);
+        });
+    }
 
     return nodeMap;
 }
@@ -133,7 +158,7 @@ function calcNodeModify(baseNodeMap: Map<string, IPublicTypeNodeSchema>, targetN
                 diffType: DiffType.Delete,
             });
         }
-        else if (!isEqual(value, targetCodeItem)) {
+        else if (!isEqual(omit(value, ['children']), omit(targetCodeItem, 'children'))) {
             diffResult.set(id, {
                 id: value.id,
                 ref: value.ref,
@@ -152,6 +177,86 @@ function calcNodeModify(baseNodeMap: Map<string, IPublicTypeNodeSchema>, targetN
             });
         }
     }
+
+    return diffResult;
+}
+
+function getNodeRef(nodeDiff: NodeDiff, diffMap: Map<string, NodeDiff>) {
+    for (const [_, value] of diffMap) {
+        if (value.ref === nodeDiff.ref && value.diffType === DiffType.Added)
+            return value;
+    }
+    return null;
+}
+
+/**
+ * 判定冲突的条件
+ * 1. 同时新增了相同 ref Node
+ * 2. 修改了同一个 node 的 ref，并且修改结果不一致
+ */
+function getNodeModifyConflict({
+    currentDiffMap,
+    nextDiffMap,
+    currentNodeMap,
+}: {
+    currentDiffMap: Map<string, NodeDiff>;
+    nextDiffMap: Map<string, NodeDiff>;
+    currentNodeMap: Map<string, IPublicTypeNodeSchema>;
+}) {
+    const conflictMap = new Map<string, CodeConflict>();
+
+    for (const [key, value] of nextDiffMap) {
+        const currentDiff = currentDiffMap.get(key);
+        if (currentDiff && currentDiff.diffType === DiffType.Updated && currentDiff.nextRef && value.nextRef && value.nextRef !== currentDiff.ref) {
+            conflictMap.set(key, {
+                key,
+                id: currentDiff.id,
+                diffType: DiffType.Updated,
+                currentNode: currentNodeMap.get(key),
+            });
+        }
+        else if (value.diffType === DiffType.Added) {
+            const currentDiff = getNodeRef(value, currentDiffMap);
+            if (currentDiff) {
+                conflictMap.set(key, {
+                    key,
+                    id: currentDiff.id,
+                    diffType: DiffType.Updated,
+                    currentNode: currentNodeMap.get(currentDiff.id),
+                });
+            }
+        }
+    }
+
+    return conflictMap;
+}
+
+function handlePageSchemaMerged({
+    baseCodeMap,
+    currentCodeMap,
+    nextCodeMap,
+
+    baseNodeMap,
+    currentNodeMap,
+    nextNodeMap,
+
+    baseSchema,
+    currentSchema,
+    nextSchema,
+}: {
+    baseCodeMap: Map<string, ICodeItem>;
+    currentCodeMap: Map<string, ICodeItem>;
+    nextCodeMap: Map<string, ICodeItem>;
+
+    baseNodeMap: Map<string, IPublicTypeNodeSchema>;
+    currentNodeMap: Map<string, IPublicTypeNodeSchema>;
+    nextNodeMap: Map<string, IPublicTypeNodeSchema>;
+
+    baseSchema: IPublicTypePageSchema;
+    currentSchema: IPublicTypePageSchema;
+    nextSchema: IPublicTypePageSchema;
+}): IPublicTypePageSchema {
+    // TODO merge
 }
 
 export function mergePageSchema(baseSchema: IPublicTypePageSchema, currentSchema: IPublicTypePageSchema, nextSchema: IPublicTypePageSchema) {
@@ -161,6 +266,11 @@ export function mergePageSchema(baseSchema: IPublicTypePageSchema, currentSchema
 
     const currentCodeDiff = calcCodeModify(baseCodeMap, currentCodeMap);
     const nextCodeDiff = calcCodeModify(baseCodeMap, nextCodeMap);
+    const codeModifyConflict = getCodeModifyConflict({
+        currentDiffMap: currentCodeDiff,
+        nextDiffMap: nextCodeDiff,
+        currentCodeMap,
+    });
 
     const baseNodeMap = genNodeMap(baseSchema);
     const currentNodeMap = genNodeMap(currentSchema);
@@ -168,4 +278,38 @@ export function mergePageSchema(baseSchema: IPublicTypePageSchema, currentSchema
 
     const currentNodeDiff = calcNodeModify(baseNodeMap, currentNodeMap);
     const nextNodeDiff = calcNodeModify(baseNodeMap, nextNodeMap);
+    const nodeModifyConflict = getNodeModifyConflict({
+        currentDiffMap: currentNodeDiff,
+        nextDiffMap: nextNodeDiff,
+        currentNodeMap,
+    });
+
+    let isConflict: boolean = false;
+    let mergedPageSchema: IPublicTypePageSchema;
+    // 没有冲突，进行 merge
+    if (codeModifyConflict.size === 0 && nodeModifyConflict.size === 0) {
+        mergedPageSchema = handlePageSchemaMerged({
+            baseCodeMap,
+            currentCodeMap,
+            nextCodeMap,
+
+            baseNodeMap,
+            currentNodeMap,
+            nextNodeMap,
+
+            baseSchema,
+            currentSchema,
+            nextSchema,
+        });
+    }
+    else {
+        isConflict = true;
+    }
+
+    return {
+        isConflict,
+        codeModifyConflict,
+        nodeModifyConflict,
+        mergedPageSchema,
+    };
 }

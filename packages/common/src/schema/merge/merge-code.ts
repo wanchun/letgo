16f0b9f939480b934df85f5e3f4dd41c381c9af8
winixt <haizekuo@gmail.com>
2024-03-31 20:54:16 +0800

    chore: 实现部分 merge 逻辑

--- packages/common/src/schema/merge/merge-code.ts
+++ packages/common/src/schema/merge/merge-code.ts
@@ -1,14 +1,14 @@
-import type { ICodeItem, ICodeStruct, IEnumCodeType, IPublicTypeNodeSchema } from '@webank/letgo-types';
-import { isEqual } from 'lodash-es';
+import type { ICodeDirectory, ICodeItem, ICodeItemOrDirectory, ICodeStruct, IEnumCodeType, IPublicTypeNodeSchema } from '@webank/letgo-types';
+import { cloneDeep, isEqual, merge, omit } from 'lodash-es';
 import { DiffType } from './merge-types';
 import type { CodeConflict } from './merge-types';
 
 interface CodeDiff {
     key: string;
     id: string;
-    type: IEnumCodeType;
     diffType: DiffType;
     nextId?: string;
+    item: ICodeItem;
 }
 
 function genCodeMap(code: ICodeStruct) {
@@ -36,17 +36,17 @@ function calcCodeModify(baseCodeMap: Map<string, ICodeItem>, targetCodeMap: Map<
             diffResult.set(key, {
                 key,
                 id: value.id,
-                type: value.type,
                 diffType: DiffType.Delete,
+                item: targetCodeItem,
             });
         }
         else if (!isEqual(value, targetCodeItem)) {
             diffResult.set(key, {
                 key,
                 id: value.id,
-                type: value.type,
                 nextId: targetCodeItem.id !== value.id ? targetCodeItem.id : undefined,
                 diffType: DiffType.Updated,
+                item: targetCodeItem,
             });
         }
     }
@@ -56,7 +56,46 @@ function calcCodeModify(baseCodeMap: Map<string, ICodeItem>, targetCodeMap: Map<
             diffResult.set(key, {
                 key,
                 id: value.id,
-                type: value.type,
+                diffType: DiffType.Added,
+                item: targetCodeMap.get(key),
+            });
+        }
+    }
+
+    return diffResult;
+}
+
+interface DirectoryDiff {
+    key: string;
+    diffType: DiffType;
+    id?: string;
+    nextId?: string;
+}
+
+function calcDirectoryModify(baseCode: ICodeStruct, targetCode: ICodeStruct) {
+    const diffResult = new Map<string, DirectoryDiff>();
+
+    for (const directory of baseCode.directories) {
+        const targetDirectory = targetCode.directories.find(item => item.key === directory.key);
+        if (!targetDirectory) {
+            diffResult.set(directory.key, {
+                key: directory.key,
+                diffType: DiffType.Delete,
+            });
+        }
+        else if (targetDirectory.id !== directory.id) {
+            diffResult.set(directory.key, {
+                key: directory.key,
+                diffType: DiffType.Updated,
+                id: directory.id,
+                nextId: targetDirectory.id,
+            });
+        }
+    }
+    for (const directory of targetCode.directories) {
+        if (!baseCode.directories.find(item => item.key === directory.key)) {
+            diffResult.set(directory.key, {
+                key: directory.key,
                 diffType: DiffType.Added,
             });
         }
@@ -67,7 +106,7 @@ function calcCodeModify(baseCodeMap: Map<string, ICodeItem>, targetCodeMap: Map<
 
 function getCodeId(codeDiff: CodeDiff, diffMap: Map<string, CodeDiff>) {
     for (const [_, value] of diffMap) {
-        if (value.id === codeDiff.id && value.type === codeDiff.type && value.diffType === DiffType.Added)
+        if (value.id === codeDiff.id && value.item.type === codeDiff.item.type && value.diffType === DiffType.Added)
             return value;
     }
     return null;
@@ -131,9 +170,115 @@ function handleCodeMerged({
     nextDiffMap: Map<string, CodeDiff>;
 }) {
     const mergedCode: ICodeStruct = {
-        code: [],
         directories: [],
+        code: [],
     };
+    const mergedCodeMap = new Map<string, ICodeItem>();
+
+    const currentDirectoryDiffMap = calcDirectoryModify(baseCode, currentCode);
+    const nextDirectoryDiffMap = calcDirectoryModify(baseCode, nextCode);
+
+    function addCodeItem(codes: ICodeItem[], codeItem: ICodeItem) {
+        if (!mergedCodeMap.has(codeItem.key)) {
+            mergedCodeMap.set(codeItem.key, codeItem);
+            codes.push(codeItem);
+        }
+    }
+
+    for (const directory of currentCode.directories) {
+        const nextDirectory = nextCode.directories.find(item => item.key === directory.key);
+        const nextDirectoryDiff = nextDirectoryDiffMap.get(directory.key);
+        const codes: ICodeItem[] = [];
+        const newDirectory: ICodeDirectory = {
+            key: directory.key,
+            id: directory.id,
+            code: codes,
+        };
+        if (nextDirectory) {
+            if (nextDirectoryDiff && nextDirectoryDiff.diffType === DiffType.Updated)
+                newDirectory.id = nextDirectoryDiff.nextId;
+
+            for (const codeItem of directory.code) {
+                const nextDiff = nextDiffMap.get(codeItem.key);
+                if (!nextDiff) {
+                    addCodeItem(codes, codeItem);
+                    continue;
+                };
+
+                const currentDiff = currentDiffMap.get(codeItem.key);
+
+                if (nextDiff.diffType === DiffType.Updated) {
+                    // TODO 修改 Id 的场景
+                    addCodeItem(codes, merge(codeItem, nextDiff.item));
+                }
+                else if (nextDiff.diffType === DiffType.Delete) {
+                    if (currentDiff && currentDiff.diffType === DiffType.Updated)
+                        addCodeItem(codes, codeItem);
+                }
+            }
+
+            // 新移入分类的
+            for (const codeItem of nextDirectory.code) {
+                if (!directory.code.find(item => item.key !== codeItem.key)) {
+                    const nextDiff = nextDiffMap.get(codeItem.key);
+                    if (!nextDiff) {
+                        addCodeItem(codes, codeItem);
+                        continue;
+                    };
+
+                    const currentDiff = currentDiffMap.get(codeItem.key);
+
+                    if (nextDiff.diffType === DiffType.Added) {
+                        let hasAdded = false;
+                        for (const cDiff of currentDiffMap.values()) {
+                            // 新增了相同的 id 并且类型一样，进行合并
+                            if (cDiff.id === codeItem.id) {
+                                hasAdded = true;
+                                addCodeItem(codes, merge(cDiff.item, codeItem));
+                            }
+                        }
+                        if (!hasAdded)
+                            addCodeItem(codes, codeItem);
+                    }
+                    if (nextDiff.diffType === DiffType.Updated) {
+                        if (currentDiff && currentDiff.diffType === DiffType.Delete)
+                            addCodeItem(codes, codeItem);
+                        else
+                            addCodeItem(codes, merge(currentDiff.item, codeItem));
+                    }
+                }
+            }
+
+            mergedCode.directories.push(newDirectory);
+        }
+        else {
+            // nextCode 删除的分类，或者是 currentCode 新增分类
+
+            // currentCode 新增分类
+            const currentDirectoryDiff = currentDirectoryDiffMap.get(directory.key);
+            if (currentDirectoryDiff) {
+                for (const codeItem of directory.code) {
+                    const nextDiff = nextDiffMap.get(codeItem.key);
+                    if (!nextDiff) {
+                        addCodeItem(codes, codeItem);
+                        continue;
+                    };
+
+                    const currentDiff = currentDiffMap.get(codeItem.key);
+
+                    if (nextDiff.diffType === DiffType.Updated) {
+                        // TODO 修改 Id 的场景
+                        addCodeItem(codes, merge(codeItem, nextDiff.item));
+                    }
+                    else if (nextDiff.diffType === DiffType.Delete) {
+                        if (currentDiff && currentDiff.diffType === DiffType.Updated)
+                            addCodeItem(codes, codeItem);
+                    }
+                }
+                mergedCode.directories.push(newDirectory);
+            }
+        }
+    }
 }
 
 export function mergeCode(baseCode: ICodeStruct, currentCode: ICodeStruct, nextCode: ICodeStruct) {

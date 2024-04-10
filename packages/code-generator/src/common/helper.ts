e924787049ca4278f99e325e495606f4dc8e5101
winixt <haizekuo@gmail.com>
2024-04-10 14:13:07 +0800

    fix: 修复出码路径问题

--- packages/code-generator/src/common/helper.ts
+++ packages/code-generator/src/common/helper.ts
@@ -148,7 +148,7 @@ function eventSchemaToFunc(ctx: Context, events: IEventHandler[] = []) {
 
 function getApiPath(api: string = '') {
     api = api.trim();
-    if (/^\w+\//.test(api))
+    if (/^[\/]?\w+\//.test(api))
         return JSON.stringify(api);
 
     return isSyntaxError(api) ? JSON.stringify(api) : api;

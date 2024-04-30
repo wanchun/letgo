a4fc602c5061d112239d1e78c5bbd193d5900b89
winixt <haizekuo@gmail.com>
2024-04-30 11:37:55 +0800

    chore: upgrade request

--- packages/template/src/plugins/plugin-code-generator/export-btn.tsx
+++ packages/template/src/plugins/plugin-code-generator/export-btn.tsx
@@ -61,7 +61,7 @@ function genProjectCodes(schema: IPublicTypeProjectSchema) {
                 'lint:fix': 'eslint . --fix',
             },
             dependencies: {
-                '@qlin/request': '0.1.8',
+                '@qlin/request': '0.2.2',
                 'core-js': '3.33.0',
                 '@fesjs/builder-vite': '3.0.2',
                 '@fesjs/fes': '3.1.4',

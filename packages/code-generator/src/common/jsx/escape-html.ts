ae34fafce59bb74a0ed6c88bc8d5d938acfec92a
qlin <haizekuo@gmail.com>
2024-08-01 19:28:00 +0800

    fix: 出码编码 html 特殊字符 (#84)

---
+++ packages/code-generator/src/common/jsx/escape-html.ts
@@ -0,0 +1,34 @@
+const escapeRE = /[<>]/;
+
+export function escapeHtml(string: unknown) {
+    const str = `${string}`;
+    const match = escapeRE.exec(str);
+
+    if (!match)
+        return str;
+
+    let html = '';
+    let escaped: string;
+    let index: number;
+    let lastIndex = 0;
+    for (index = match.index; index < str.length; index++) {
+        switch (str.charCodeAt(index)) {
+            case 60: // <
+                escaped = '&lt;';
+                break;
+            case 62: // >
+                escaped = '&gt;';
+                break;
+            default:
+                continue;
+        }
+
+        if (lastIndex !== index)
+            html += str.slice(lastIndex, index);
+
+        lastIndex = index + 1;
+        html += escaped;
+    }
+
+    return lastIndex !== index ? html + str.slice(lastIndex, index) : html;
+}

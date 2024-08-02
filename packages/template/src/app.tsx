9e304792d660d6e676d97391ac106da76a5ea79c
harrywan <harrywan@webank.com>
2024-08-02 14:16:39 +0800

    fix: 去掉多余带阿米

--- packages/template/src/app.tsx
+++ packages/template/src/app.tsx
@@ -1,15 +1,5 @@
 import { defineRuntimeConfig } from '@fesjs/fes';
 import { FConfigProvider } from '@fesjs/fes-design';
-import { plugins } from '@webank/letgo-engine';
-import PluginDevice from '@webank/letgo-plugin-device';
-import PluginSchema from '@webank/letgo-plugin-schema';
-import PluginUndoRedo from '@webank/letgo-plugin-undo-redo';
-import PluginCSS from '@webank/letgo-plugin-css';
-import PluginLogicEdit from '@webank/letgo-plugin-logic';
-import PluginConsole from '@webank/letgo-plugin-console';
-import PluginLogo from './plugins/plugin-logo';
-import PluginPreview from './plugins/plugin-preview-sample';
-import PluginCodeGenerator from './plugins/plugin-code-generator';
 
 // const injectGroup: IPublicTypeFieldConfig = {
 //     name: 'action',
@@ -33,25 +23,6 @@ import PluginCodeGenerator from './plugins/plugin-code-generator';
 //         metadata.configure.props.push(injectGroup);
 //     return metadata;
 // }, 100, 'inject-custom');
-plugins.register(PluginCSS);
-plugins.register(PluginLogicEdit);
-plugins.register(PluginDevice);
-plugins.register(PluginSchema);
-plugins.register(PluginLogo);
-plugins.register(PluginUndoRedo, {
-    area: 'topArea',
-});
-plugins.register(PluginPreview);
-plugins.register(PluginCodeGenerator);
-plugins.register(PluginConsole);
-
-// setTimeout(async () => {
-//     await plugins.delete(PluginCSS.name);
-// }, 3000);
-
-// setTimeout(async () => {
-//     await plugins.register(PluginCSS, {}, { autoInit: true });
-// }, 6000);
 
 export default defineRuntimeConfig({
     rootContainer(Container: any) {
